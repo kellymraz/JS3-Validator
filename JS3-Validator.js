26988c2c715313b8da1e931a403e21fe96d3f043
Student <kitty8mars@gmail.com>
2015-06-23 15:58:34 -0600

    Actually seemed easy

---
+++ JS3-Validator.js
@@ -0,0 +1,25 @@
+
+	// window.prompt("Please enter your phone number including dashes.  
+	// We promise not to share it with anyone.");
+
+// var phoneNumber = prompt("Please enter your phone number including dashes. We promise not to share it with anyone.");
+// 	console.log(phoneNumber);
+// 	alert(phoneNumber.charAt(4) === "-")
+// 		// &&& charAt(7) === "-"
+ 
+// var birthDate = prompt("Pease enter your date of birth using the following format: mm/dd/yy");
+// 	console.log(birthDate);
+// 	alert(birthDate.charAt(3) === "-"
+
+
+
+// var postalCode = prompt("Please enter your 5-digit postal code");
+// 	console.log(postalCode);
+// 	alert(postalCode.length === 5)
+
+// var stateAbbrev = prompt("Please enter the 2-letter abbreviation for your state. ex. CO for Colorado")
+// 	console.log(stateAbbrev);
+// 	alert(stateAbbrev.length === 2 && stateAbbrev.toUpperCase() === stateAbbrev)
+
+var married = prompt("Are you married? Please enter 'Yes' or 'No'")
+	console.log(married);
